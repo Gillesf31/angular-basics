1ab177f354b6335e1fbda74b511fd036a335d8c6
Gilles Ferrand <gilles.ferrand@ittingbox.com>
2022-09-29 17:30:45 +0200

    ngStyle

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -4,7 +4,11 @@ import {Donut} from "../../models/donut.model";
 @Component({
   selector: 'app-donut-card',
   template: `
-    <div class="donut-card">
+    <div class="donut-card" 
+      [ngStyle]="{
+        border: donut.promo ? '2px solid #eee': 'none'
+      }"
+      >
       <img
         src="/assets/img/{{  donut.icon }}.svg"
         [alt]="donut.name"
@@ -12,11 +16,36 @@ import {Donut} from "../../models/donut.model";
       />
       <div>
         <p class="donut-card-name">{{ donut.name }}</p>
-        <p class="donut-card-price">{{ donut.price }}</p>
+        <p class="donut-card-price">{{ donut.price / 100 | currency: 'USD' }}</p>
       </div>
     </div>
   `,
   styles: [
+    `
+    .donut-card {
+         display: flex;
+        align-items: center;
+        background: #f7f7f7;
+        border-radius: 5px;
+        margin-bottom: 5px;
+        padding: 5px 15px;
+        transform: transform 0.2s ease-in-out;
+        &:hover {
+          transform: translateY(-3px);
+        }
+        &-name {
+          font-size: 16px;
+        }
+        &-price {
+          font-size: 14 px;
+          color: #c14583;
+        }
+        &-icon {
+          width: 50px;
+          margin-right: 10px;
+        }
+      }
+    `,
   ]
 })
 export class DonutCardComponent {
