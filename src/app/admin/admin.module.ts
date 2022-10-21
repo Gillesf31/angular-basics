9910c741011de663b1635db70769fbc14ab7a54e
Gilles <gilles.ferrand31@gmail.com>
2022-10-21 08:09:03 +0200

    Forms

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+// Containers
 import { DonutListComponent } from './containers/donut-list/donut-list.component';
+import { DonutSingleComponent } from './containers/donut-single/donut-single.component';
+
+// Components
 import { DonutCardComponent } from './components/donut-card/donut-card.component';
+import { DonutFormComponent } from './components/donut-form/donut-form.component';
 
+// Services
 
+// Guards
+
+// Directives
 
 @NgModule({
   declarations: [
     DonutListComponent,
-    DonutCardComponent
+    DonutSingleComponent,
+    DonutCardComponent,
+    DonutFormComponent
   ],
   exports: [
-    DonutListComponent
+    DonutListComponent,
+    DonutSingleComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class AdminModule { }
---
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donut-single',
+  template: `
+    <div>
+      <app-donut-form></app-donut-form>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class DonutSingleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
