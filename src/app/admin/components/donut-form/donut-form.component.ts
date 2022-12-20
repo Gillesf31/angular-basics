7f64903551f9815e90d5dd4a10768ea6a07edda0
Gilles Ferrand <gilles.ferrand31@gmail.com>
2022-12-20 17:09:01 +0100

    Show Error Messages on Validation Change

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -6,13 +6,11 @@ import { Component, OnInit } from '@angular/core';
     <form class="donut-form" #form="ngForm">
       <label>
         <span>Name</span>
-        <input type="text" name="name" class="input" required ngModel #name="ngModel">
-        <p>{{ name.valid }}</p>
-        <p>{{ name.invalid }}</p>
-        <p>{{ name.touched }}</p>
-        <p>{{ name.untouched }}</p>
-        <p>{{ name.pristine }}</p>
-        <p>{{ name.dirty }}</p>
+        <input type="text" name="name" class="input" required minlength="5" ngModel #name="ngModel">
+        <ng-container *ngIf="name.invalid && name.touched">
+          <div class="donut-form-error" *ngIf="name.errors?.required">Name is required.</div>
+          <div class="donut-form-error" *ngIf="name.errors?.minlength">Name is too short.</div>
+        </ng-container>
       </label>
       <label>
         <span>Icon</span>
@@ -67,6 +65,10 @@ import { Component, OnInit } from '@angular/core';
               margin-bottom: 0;
             }
           }
+        }
+        &-error {
+          font-size: 12px;
+          color: #e66262;
         }
       }
     `
