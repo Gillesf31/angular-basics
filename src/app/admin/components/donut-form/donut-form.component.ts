8411c09d5460f6fde87978575fe175b4232d770b
Gilles Ferrand <gilles.ferrand31@gmail.com>
2022-12-22 11:16:04 +0100

    One-Way Data Binding to NgModel

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, Output, EventEmitter} from '@angular/core';
+import {Component, Output, EventEmitter, Input} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {Donut} from "../../models/donut.model";
 
@@ -8,7 +8,7 @@ import {Donut} from "../../models/donut.model";
     <form class="donut-form" (ngSubmit)="handleSubmit(form)" #form="ngForm">
       <label>
         <span>Name</span>
-        <input type="text" name="name" class="input" required minlength="5" ngModel [ngModelOptions]="{ updateOn: 'blur'}" #name="ngModel">
+        <input type="text" name="name" class="input" required minlength="5" [ngModel]="donut.name" [ngModelOptions]="{ updateOn: 'blur'}" #name="ngModel">
         <ng-container *ngIf="name.invalid && name.touched">
           <div class="donut-form-error" *ngIf="name.errors?.required">Name is required.</div>
           <div class="donut-form-error" *ngIf="name.errors?.minlength">Name is too short.</div>
@@ -16,7 +16,7 @@ import {Donut} from "../../models/donut.model";
       </label>
       <label>
         <span>Icon</span>
-        <select name="icon" class="input input--select" required ngModel #icon="ngModel">
+        <select name="icon" class="input input--select" required [ngModel]="donut.icon" #icon="ngModel">
           <option *ngFor="let icon of icons" [ngValue]="icon">{{icon}}</option>
         </select>
         <ng-container *ngIf="icon.invalid && icon.touched">
@@ -25,7 +25,7 @@ import {Donut} from "../../models/donut.model";
       </label>
       <label>
         <span>Price</span>
-        <input type="number" name="price" class="input" required ngModel #price="ngModel">
+        <input type="number" name="price" class="input" required [ngModel]="donut.price" #price="ngModel">
         <ng-container *ngIf="price.invalid && price.touched">
           <div class="donut-form-error" *ngIf="price.errors?.required">Price is required.</div>
         </ng-container>
@@ -34,15 +34,15 @@ import {Donut} from "../../models/donut.model";
       <div class="donut-form-radios">
         <p class="donut-form-radios-label">Promo:</p>
         <label>
-          <input type="radio" name="promo" [value]="undefined" ngModel>
+          <input type="radio" name="promo" [value]="undefined" [ngModel]="donut.promo">
           <span>None</span>
         </label>
         <label>
-          <input type="radio" name="promo" value="new" ngModel>
+          <input type="radio" name="promo" value="new" [ngModel]="donut.promo">
           <span>New</span>
         </label>
         <label>
-          <input type="radio" name="promo" value="limited" ngModel>
+          <input type="radio" name="promo" value="limited" [ngModel]="donut.promo">
           <span>Limited</span>
         </label>
 
@@ -50,7 +50,7 @@ import {Donut} from "../../models/donut.model";
 
       <label>
         <span>Description</span>
-        <textarea name="description" class="input input--textarea" required ngModel #description="ngModel"></textarea>
+        <textarea name="description" class="input input--textarea" required [ngModel]="donut.description" #description="ngModel"></textarea>
         <ng-container *ngIf="description.invalid && description.touched">
           <div class="donut-form-error" *ngIf="description.errors?.required">Description is required.</div>
         </ng-container>
@@ -65,6 +65,7 @@ import {Donut} from "../../models/donut.model";
 
       <!-- Debug purpose -->
       <pre>{{ form.value | json }}</pre>
+      <pre>{{ donut | json }}</pre>
     </form>
   `,
   styles: [
@@ -99,6 +100,7 @@ import {Donut} from "../../models/donut.model";
   ]
 })
 export class DonutFormComponent {
+  @Input() donut!: Donut;
   @Output() create = new EventEmitter<Donut>();
 
   icons: string[] = [
--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -5,17 +5,26 @@ import {Donut} from "../../models/donut.model";
   selector: 'app-donut-single',
   template: `
     <div>
-      <app-donut-form (create)="onCreate($event)"></app-donut-form>
+      <app-donut-form [donut]="donut" (create)="onCreate($event)"></app-donut-form>
     </div>
   `,
   styles: [
   ]
 })
 export class DonutSingleComponent implements OnInit {
+  donut!: Donut;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.donut = {
+      id: 'y8z0As',
+      name: 'Just Chocolate',
+      icon: 'just-chocolate',
+      price: 119,
+      promo: 'new',
+      description: 'For  the pure chocoholic.',
+    };
   }
 
   onCreate(donut: Donut) {
