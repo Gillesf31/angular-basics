83eaa300b8c8978c1ae505beccf9b89ee814a296
Gilles Ferrand <gilles.ferrand31@gmail.com>
2022-12-22 11:06:09 +0100

    Emit Form Payload to Smart Container via @Output

--- src/app/admin/containers/donut-single/donut-single.component.ts
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {Donut} from "../../models/donut.model";
 
 @Component({
   selector: 'app-donut-single',
   template: `
     <div>
-      <app-donut-form></app-donut-form>
+      <app-donut-form (create)="onCreate($event)"></app-donut-form>
     </div>
   `,
   styles: [
@@ -15,6 +16,10 @@ export class DonutSingleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(donut: Donut) {
+    console.log(donut);
   }
 
 }
